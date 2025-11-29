44be6ec7b621be44bf195785c75837184d4ebf94
Christopher Clark <cclark2@iwu.edu>
2025-11-29 13:03:08 -0600

    added some more testing

--- Unittest.py
+++ Unittest.py
@@ -16,21 +16,31 @@ class FlaskrTestCase(unittest.TestCase):
         os.close(self.db_fd)
         os.unlink(flaskr.app.config['DATABASE'])
     
-    def register(self, username, email, password):
+    def test_register(self, username, email, password):
         return self.app.post('/register', data=dict(
             username=username,
             email=email,
             password=password
         ), follow_redirects=True)
 
-    def login(self, username, password):
+    def test_login(self, username, password):
         return self.app.post('/login', data=dict(
         username=username,
         password=password
     ), follow_redirects=True)
 
-    def logout(self):
+    def test_logout(self):
         return self.app.get('/logout', follow_redirects=True)
+
+    def test_login_logout(self):
+        rv = self.test_login('admin', 'default')
+        assert b'You were logged in' in rv.data
+        rv = self.test_logout()
+        assert b'You were logged out' in rv.data
+        rv = self.test_login('adminx', 'default')
+        assert b'Invalid username' in rv.data
+        rv = self.test_login('admin', 'defaultx')
+        assert b'Invalid password' in rv.data
 
 if __name__ == '__main__':
     unittest.main()
