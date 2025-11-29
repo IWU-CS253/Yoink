c107eb299b70da8e149c5a7bba2a9f56a71e9d59
Christopher Clark <cclark2@iwu.edu>
2025-11-29 13:12:38 -0600

    added register testing

--- Unittest.py
+++ Unittest.py
@@ -16,30 +16,34 @@ class FlaskrTestCase(unittest.TestCase):
         os.close(self.db_fd)
         os.unlink(flaskr.app.config['DATABASE'])
     
-    def test_register(self, username, email, password):
+    def register(self, username, email, password):
         return self.app.post('/register', data=dict(
             username=username,
             email=email,
             password=password
         ), follow_redirects=True)
 
-    def test_login(self, username, password):
+    def login(self, username, password):
         return self.app.post('/login', data=dict(
         username=username,
         password=password
     ), follow_redirects=True)
 
-    def test_logout(self):
+    def logout(self):
         return self.app.get('/logout', follow_redirects=True)
 
+    def test_register(self):
+        ra = self.register('admin', 'admin@iwu.edu', 'default')
+        assert b'You were registered' in ra.data
+
     def test_login_logout(self):
-        rv = self.test_login('admin', 'default')
+        rv = self.login('admin', 'default')
         assert b'You were logged in' in rv.data
-        rv = self.test_logout()
+        rv = self.logout()
         assert b'You were logged out' in rv.data
-        rv = self.test_login('adminx', 'default')
+        rv = self.login('adminx', 'default')
         assert b'Invalid username' in rv.data
-        rv = self.test_login('admin', 'defaultx')
+        rv = self.login('admin', 'defaultx')
         assert b'Invalid password' in rv.data
 
 if __name__ == '__main__':
